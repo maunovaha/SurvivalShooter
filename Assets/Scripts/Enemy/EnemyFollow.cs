064e06bc3729fbd8b56d339141bf3bf11f15917a
Mauno Vähä <maunovaha@gmail.com>
2019-02-06 07:21:17 +0200

    Makes enemy to follow the player

---
+++ Assets/Scripts/Enemy/EnemyFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Tutorial
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class EnemyFollow : MonoBehaviour
+    {
+        private Transform player;
+        private NavMeshAgent agent;
+
+        private void Start()
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update()
+        {
+            agent.SetDestination(player.position);
+        }
+    }
+}
+
